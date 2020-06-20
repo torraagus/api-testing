6729c047c153d931ba68cd07f09f8db6f0a566b8
broana-ait <torra.agustinignacio@gmail.com>
2020-06-20 19:38:27 -0300

    create test for usersApi mocking axios

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,36 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import mockAxios from "axios";
+import { usersApi } from "./services/users-api";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("api testing", () => {
+	test("should call axios and return users", async () => {
+		mockAxios.get.mockImplementationOnce(() =>
+			Promise.resolve({
+				data: [
+					{
+						_id: "1010201",
+						name: "Federico",
+					},
+					{
+						_id: "1010253",
+						name: "Manuel",
+					},
+				],
+			})
+		);
+
+		const users = await usersApi();
+		expect(mockAxios.get).toHaveBeenCalledTimes(1);
+		expect(mockAxios.get).toHaveBeenCalledWith(
+			"http://localhost:3001/api/users"
+		);
+		expect(users.length).toBe(2);
+		expect(users[0]._id).toEqual("1010201");
+		expect(users[0].name).toEqual("Federico");
+	});
 });
+
+// test("renders learn react link", () => {
+// 	const { getByText } = render(<App />);
+// 	const linkElement = getByText(/learn react/i);
+// 	expect(linkElement).toBeInTheDocument();
+// });
